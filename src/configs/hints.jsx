5330a44731e00c0fa8b28e8ca7adf84bd9b2794a
MinavKaria <minavpkaria@gmail.com>
2025-04-08 00:12:42 +0530

    add changes

--- src/configs/hints.jsx
+++ src/configs/hints.jsx
@@ -10,13 +10,13 @@ const hints=[
     },
     {
         text:"Q4",
-        url:"https://firebasestorage.googleapis.com/v0/b/acm-fy-rep-login.appspot.com/o/cipherhint.png?alt=media&token=f0eb68fe-616c-4b71-bf31-eefa166c006a"
+        url:"https://firebasestorage.googleapis.com/v0/b/acm-fy-rep-login.appspot.com/o/cipheryhint.png?alt=media&token=72a160a3-d2e7-4b47-be00-11fc362219e0"
     },
     {
         text:"Q5: Check the properties of the tree, and try to solve using that pattern.",
     },
     {
-        text:"Q6",
+        text:"Q6: Yo bro find me the pairs of x and y values which can exist for following",
         url:"https://firebasestorage.googleapis.com/v0/b/acm-fy-rep-login.appspot.com/o/Brainrot%20tables%20(1).png?alt=media&token=42e237a3-55aa-4e79-8630-41a949523e98"
     },
     {
